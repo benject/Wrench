27d5a1b3324bd1d8013579bfc0e649e8cb83f609
Bing Chen <benject@hotmail.com>
2022-10-29 21:14:45 +0800

    update common tools

---
+++ src/common/__init__.py
@@ -0,0 +1,9 @@
+
+# for ui
+from maya import OpenMayaUI as omui 
+from PySide2.QtCore import * 
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from PySide2.QtUiTools import QUiLoader
+
+from shiboken2 import wrapInstance 
